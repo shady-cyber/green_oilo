7122dcffd79ce2d8114310df7177133f34adbb2b
shady-cyber <shadyomar30@gmail.com>
2024-11-17 12:26:41 +0200

    last web deployment added

--- lib/business/connection/backend/end_points.dart
+++ lib/business/connection/backend/end_points.dart
@@ -1,5 +1,5 @@
 class EndPoints {
-  static const BASE_URL = 'http://185.182.184.59';
+  static const BASE_URL = 'https://renioil.com';
   static const SUB_URL = '/api/delivery-boy';
   static const GET_ALL = '/get-all/';
   static const UPDATE_ORDER = '/update-order-status';
